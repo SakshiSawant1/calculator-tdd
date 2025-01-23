9692f613542f0863a67ecb66c03c7cc4ba89546f
sakshisawnt1 <sakshisawnt22520@gmail.com>
2025-01-23 08:30:11 +0530

    feat:Add default delimiter

--- stringsum_tdd.py
+++ stringsum_tdd.py
@@ -3,4 +3,5 @@ class StringCalculator:
     A class to calculate the sum of numbers from a given string.
     Supports custom delimiters and handles various input formats.
     """
-
+    def __init__(self):
+        self.delimiter = ','
